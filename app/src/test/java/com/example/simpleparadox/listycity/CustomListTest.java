f4083f9e204186f9fed0b501d5e7732a4b670acc
YijieGu <73412566+YijieGu@users.noreply.github.com>
2021-10-31 01:28:50 -0600

    Red: deleteCity test failed (should fail)

--- app/src/test/java/com/example/simpleparadox/listycity/CustomListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.simpleparadox.listycity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -32,5 +33,21 @@ public class CustomListTest {
         City newCity2 = new City("Toronto","ON");
         assertFalse(list.hasCity(newCity2));
     }
+
+    @Test
+    public  void deleteCityTest(){
+        City newCity = new City("Halifax","Ns");
+        list.addCity(newCity);
+        assertEquals(list.getCount(),1);
+        list.deleteCity(newCity);
+        assertEquals(list.getCount(),0);
+        City newCity2 = new City("Toronto","ON");
+        assertThrows(IllegalArgumentException.class, () -> {
+            list.deleteCity(newCity2);
+        });
+    }
+
+
+
 }
 
